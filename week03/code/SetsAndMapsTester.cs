ec2d96e2933ac1758f6d1ecb43dfa122a7e5b41d
alekskahawai <vol17004@byui.edu>
2024-05-16 17:48:11 +0100

    w03

--- week03/code/SetsAndMapsTester.cs
+++ week03/code/SetsAndMapsTester.cs
@@ -1,7 +1,9 @@
 using System.Text.Json;
 
-public static class SetsAndMapsTester {
-    public static void Run() {
+public static class SetsAndMapsTester
+{
+    public static void Run()
+    {
         // Problem 1: Find Pairs with Sets
         Console.WriteLine("\n=========== Finding Pairs TESTS ===========");
         DisplayPairs(new[] { "am", "at", "ma", "if", "fi" });
@@ -89,11 +91,9 @@ public static class SetsAndMapsTester {
         // 4km SW of Volcano, Hawaii - Mag 1.99
     }
 
+    // Problem 1: Find Pairs with Sets
     /// <summary>
-    /// The words parameter contains a list of two character 
-    /// words (lower case, no duplicates). Using sets, find an O(n) 
-    /// solution for displaying all symmetric pairs of words.  
-    ///
+    /// The words parameter contains a list of two character words (lower case, no duplicates). Using sets, find an O(n) solution for displaying all symmetric pairs of words.  
     /// For example, if <c>words</c> was: <c>[am, at, ma, if, fi]</c>, we would display:
     /// <code>
     /// am &amp; ma
@@ -102,69 +102,120 @@ public static class SetsAndMapsTester {
     /// The order of the display above does not matter. <c>at</c> would not 
     /// be displayed because <c>ta</c> is not in the list of words.
     ///
-    /// As a special case, if the letters are the same (example: 'aa') then
-    /// it would not match anything else (remember the assumption above
-    /// that there were no duplicates) and therefore should not be displayed.
+    /// As a special case, if the letters are the same (example: 'aa') then it would not match anything else (remember the assumption above that there were no duplicates) and therefore should not be displayed.
     /// </summary>
     /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
-    private static void DisplayPairs(string[] words) {
-        // To display the pair correctly use something like:
-        // Console.WriteLine($"{word} & {pair}");
-        // Each pair of words should displayed on its own line.
+    private static void DisplayPairs(string[] words)
+    {
+        // initiate a new set to keep track of values seen
+        var valueSeen = new HashSet<string>();
+
+        // iterate through all values in 'words' collection
+        foreach (var word in words)
+        {
+            // reverse each value
+            var tail = word[0].ToString();
+            var pair = word[1].ToString();
+            pair += tail;
+            // var pair = word.ToCharArray().Reverse();
+            // var pair = new string(word.ToCharArray().Reverse().ToArray());
+
+            // check if we already saw the 'pair' value 
+            // To display the pair correctly use something like:
+            // Console.WriteLine($"{word} & {pair}");
+            // Each pair of words should displayed on its own line.
+            // if (valueSeen.Contains(word.Reverse<string>())
+            if (valueSeen.Contains(pair))
+                Console.WriteLine($"{word} & {pair}");
+
+            // populate the set to keep track of values seen
+            valueSeen.Add(word);
+        }
     }
 
+
+    // Problem 2: Degree Summary
     /// <summary>
-    /// Read a census file and summarize the degrees (education)
-    /// earned by those contained in the file.  The summary
-    /// should be stored in a dictionary where the key is the
-    /// degree earned and the value is the number of people that 
-    /// have earned that degree.  The degree information is in
-    /// the 4th column of the file.  There is no header row in the
-    /// file.
+    /// Read a census file and summarize the degrees (education) earned by those contained in the file.  The summary should be stored in a dictionary where the key is the degree earned and the value is the number of people that have earned that degree.  The degree information is in the 4th column of the file. There is no header row in the file.
     /// </summary>
     /// <param name="filename">The name of the file to read</param>
     /// <returns>fixed array of divisors</returns>
-    /// #############
-    /// # Problem 2 #
-    /// #############
-    private static Dictionary<string, int> SummarizeDegrees(string filename) {
+    private static Dictionary<string, int> SummarizeDegrees(string filename)
+    {
         var degrees = new Dictionary<string, int>();
-        foreach (var line in File.ReadLines(filename)) {
+        foreach (var line in File.ReadLines(filename))
+        {
             var fields = line.Split(",");
             // Todo Problem 2 - ADD YOUR CODE HERE
+
+            // initiate a variable to get the degree's name in the line fields[3]
+            var degreeName = fields[3];
+            //var count = 1;
+
+            // check 'degrees' dictionary
+            // if contains the degree, increase counter by 1
+            if (degrees.ContainsKey(degreeName))
+                //degrees[degreeName] += count;
+                degrees[degreeName] += 1;
+
+            // else add the degree to the dictionary, set counter to 1
+            else
+                //degrees[degreeName] = count;
+                degrees[degreeName] = 1;
         }
 
         return degrees;
     }
 
+    
+    // Problem 3: Anagrams
     /// <summary>
-    /// Determine if 'word1' and 'word2' are anagrams.  An anagram
-    /// is when the same letters in a word are re-organized into a 
-    /// new word.  A dictionary is used to solve the problem.
-    /// 
+    /// Determine if 'word1' and 'word2' are anagrams.  An anagram is when the same letters in a word are re-organized into a new word.  A dictionary is used to solve the problem.
     /// Examples:
     /// is_anagram("CAT","ACT") would return true
     /// is_anagram("DOG","GOOD") would return false because GOOD has 2 O's
     /// 
-    /// Important Note: When determining if two words are anagrams, you
-    /// should ignore any spaces.  You should also ignore cases.  For 
-    /// example, 'Ab' and 'Ba' should be considered anagrams
+    /// Important Note: When determining if two words are anagrams, you should ignore any spaces.  You should also ignore cases.  For example, 'Ab' and 'Ba' should be considered anagrams
     /// 
-    /// Reminder: You can access a letter by index in a string by 
-    /// using the [] notation.
+    /// Reminder: You can access a letter by index in a string by using the [] notation.
     /// </summary>
-    /// #############
-    /// # Problem 3 #
-    /// #############
-    private static bool IsAnagram(string word1, string word2) {
+    private static bool IsAnagram(string word1, string word2)
+    {
         // Todo Problem 3 - ADD YOUR CODE HERE
         return false;
     }
 
+
+    // Problem 5: Earthquake
+    /// <summary>
+    /// This function will read JSON (Javascript Object Notation) data from the United States Geological Service (USGS) consisting of earthquake data. The data will include all earthquakes in the current day.
+    /// JSON data is organized into a dictionary. After reading the data using the built-in HTTP client library, this function will print out a list of all earthquake locations ('place' attribute) and magnitudes ('mag' attribute). Additional information about the format of the JSON data can be found at this website: https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php  </summary>
+    private static void EarthquakeDailySummary()
+    {
+        const string uri = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+        using var client = new HttpClient();
+        using var getRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
+        using var jsonStream = client.Send(getRequestMessage).Content.ReadAsStream();
+        using var reader = new StreamReader(jsonStream);
+        var json = reader.ReadToEnd();
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+        var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
+
+        // TODO:
+        // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties on those classes so that the call to Deserialize above works properly.
+
+        // 2. Add code below to print out each place a earthquake has happened today and its magitude.
+    }
+
+
+
+    // Problem 4: Maze
     /// <summary>
     /// Sets up the maze dictionary for problem 4
     /// </summary>
-    private static Dictionary<ValueTuple<int, int>, bool[]> SetupMazeMap() {
+    private static Dictionary<ValueTuple<int, int>, bool[]> SetupMazeMap()
+    {
         Dictionary<ValueTuple<int, int>, bool[]> map = new() {
             { (1, 1), new[] { false, true, false, true } },
             { (1, 2), new[] { false, true, true, false } },
@@ -206,34 +257,4 @@ public static class SetsAndMapsTester {
         return map;
     }
 
-    /// <summary>
-    /// This function will read JSON (Javascript Object Notation) data from the 
-    /// United States Geological Service (USGS) consisting of earthquake data.
-    /// The data will include all earthquakes in the current day.
-    /// 
-    /// JSON data is organized into a dictionary. After reading the data using
-    /// the built-in HTTP client library, this function will print out a list of all
-    /// earthquake locations ('place' attribute) and magnitudes ('mag' attribute).
-    /// Additional information about the format of the JSON data can be found 
-    /// at this website:  
-    /// 
-    /// https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
-    /// 
-    /// </summary>
-    private static void EarthquakeDailySummary() {
-        const string uri = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
-        using var client = new HttpClient();
-        using var getRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
-        using var jsonStream = client.Send(getRequestMessage).Content.ReadAsStream();
-        using var reader = new StreamReader(jsonStream);
-        var json = reader.ReadToEnd();
-        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-
-        var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
-
-        // TODO:
-        // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
-        // on those classes so that the call to Deserialize above works properly.
-        // 2. Add code below to print out each place a earthquake has happened today and its magitude.
-    }
 }
